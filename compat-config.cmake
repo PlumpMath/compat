eacacec6ddca416cbe1f2842d2275ebc581f0ad8
Alexej Harm <aharm@xdsnet.de>
2016-11-16 10:59:21 +0100

    msvc & clang support

--- compat-config.cmake
+++
@@ -1,8 +0,0 @@
-set(COMPAT_FOUND true)
-set(COMPAT_VERSION "4.0.0")
-
-if(NOT TARGET compat)
-  add_library(compat INTERFACE IMPORTED)
-  set_target_properties(compat PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include")
-endif()
